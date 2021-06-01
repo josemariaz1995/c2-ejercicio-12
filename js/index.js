6ce5ca0f6a46d4410d3937192709edc351b6745e
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-01 11:45:24 +0200

    ejercicio acabado

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const numeroAleatorio = Math.floor(Math.random() * 11);
+console.log(numeroAleatorio);
+const numeroRecibido = document.querySelector(".numero");
+const adivina = document.querySelector(".adivinar");
+const mensaje = document.querySelector(".mensaje");
+adivina.addEventListener("click", (e) => {
+  console.log(numeroRecibido.value);
+  if (+numeroRecibido.value === +numeroAleatorio) {
+    adivina.classList.add("acierto");
+    adivina.disabled = true;
+    mensaje.textContent = "acertado";
+  } else {
+    e =
+      +numeroRecibido.value > +numeroAleatorio
+        ? (mensaje.textContent = `el numero es menor de ${numeroRecibido.value} `)
+        : (mensaje.textContent = `el numero es mayor de ${numeroRecibido.value} `);
+  }
+});
